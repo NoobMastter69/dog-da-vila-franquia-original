7572a14dd6492dba9875d83d2271df7a3fdd61d6
ed carlos <edcarlosmegatrom@gmail.com>
2025-06-07 18:43:52 -0300

    logo atuali 2.5

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -21,17 +21,17 @@ const HeroSection = () => {
         <img
           src="/images/logo2.PNG"
           alt="Dog da Vila"
-          className="w-56 sm:w-72 md:w-80 mb-4 animate-fade-in mt-16"
+          className="w-56 sm:w-72 md:w-80 mt-8 mb-2 animate-fade-in"
         />
-        <h1 className="text-2xl sm:text-3xl md:text-4xl text-white font-bold mb-4 animate-fade-in">
+        <h1 className="text-2xl sm:text-3xl md:text-4xl text-white font-bold mb-2 animate-fade-in">
           Simplesmente Diferente
         </h1>
-        <div className="mt-6 animate-fade-in flex justify-center">
+        <div className="mt-1 animate-fade-in flex justify-center">
           <Button
             className="bg-orange-500 hover:bg-orange-600 text-white rounded-full
-                       text-xl py-10 px-10           /* Base (Celular): Tamanho perfeito, mantido! */
-                       md:text-2xl md:py-12 md:px-16   /* MD (Tablet): Transição suave */
-                       lg:text-3xl lg:py-16 lg:px-28   /* LG (PC): Altura aumentada sutilmente para 16 */
+                       text-xl py-10 px-10
+                       md:text-2xl md:py-12 md:px-16
+                       lg:text-3xl lg:py-16 lg:px-28
                        shadow-xl
                        transition-transform duration-200
                        hover:scale-105"
@@ -46,3 +46,4 @@ const HeroSection = () => {
 };
 
 export default HeroSection;
+
